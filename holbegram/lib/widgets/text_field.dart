1ad623e1a9f9f67d8f1da2ef5bea7c349caefcc3
ali-jin <alina.jin.contact@gmail.com>
2025-07-22 10:55:24 +0200

    start prject

---
+++ holbegram/lib/widgets/text_field.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class TextFieldInput extends StatelessWidget {
+  final TextEditingController controller;
+  final bool isPassword;
+  final String hintText;
+  final Widget? suffixIcon;
+  final TextInputType keyboardType;
+
+  const TextFieldInput({
+    Key? key,
+    required this.controller,
+    required this.isPassword,
+    required this.hintText,
+    this.suffixIcon,
+    required this.keyboardType,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      keyboardType: keyboardType,
+      controller: controller,
+      cursorColor: const Color.fromARGB(218, 226, 37, 24),
+      textInputAction: TextInputAction.next,
+      obscureText: isPassword,
+      decoration: InputDecoration(
+        hintText: hintText,
+        border: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.transparent,
+            style: BorderStyle.none,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(
+            color: Colors.transparent,
+            style: BorderStyle.none,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: const BorderSide(
+            color: Colors.transparent,
+            style: BorderStyle.none,
+          ),
+        ),
+        filled: true,
+        contentPadding: const EdgeInsets.all(8),
+        suffixIcon: suffixIcon,
+      ),
+    );
+  }
+}
