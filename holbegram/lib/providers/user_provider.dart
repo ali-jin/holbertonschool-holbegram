377bfcbd1e15e5726b6d0a5f07c451114777be1d
ali-jin <alina.jin.contact@gmail.com>
2025-07-23 16:10:27 +0200

    create providers folder with user_provider.dart

---
+++ holbegram/lib/providers/user_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import '../methods/auth_methods.dart';
+import '../models/user.dart';
+
+class UserProvider with ChangeNotifier {
+  Users? _user;
+  final AuthMethode _authMethode = AuthMethode();
+
+  Users get user => _user!;
+
+  Future<void> refreshUser() async {
+    Users user = await _authMethode.getUserDetails();
+    _user = user;
+    notifyListeners();
+  }
+}
